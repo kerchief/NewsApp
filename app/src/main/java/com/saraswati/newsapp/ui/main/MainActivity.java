0f44036e59301af6c6d1424056459d9e18dd9c3a
saraswatisc <sarascahya@gmail.com>
2018-01-14 13:45:07 +0800

    finish layout with dummy data

--- app/src/main/java/com/saraswati/newsapp/ui/main/MainActivity.java
+++ app/src/main/java/com/saraswati/newsapp/ui/main/MainActivity.java
@@ -2,9 +2,14 @@ package com.saraswati.newsapp.ui.main;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.saraswati.newsapp.R;
+import com.saraswati.newsapp.data.remote.model.NewsItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -13,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.rv_news)
     RecyclerView rvNews;
+    private NewsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        rvNews.setAda
+        //membuat adapter
+        adapter = new NewsAdapter();
+        //set data
+        adapter.setData(getDummyData());
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        rvNews.setLayoutManager(linearLayoutManager);
+        rvNews.setAdapter(adapter);
+    }
+
+    public List<NewsItem> getDummyData(){
+        List<NewsItem> result = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            NewsItem newsItem = new NewsItem();
+            newsItem.setTitle("Judul "+ String.valueOf(i)); //judul
+            newsItem.setAuthor("Penulis "+ String.valueOf(i)); //judul
+            newsItem.setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." + String.valueOf(i));
+            newsItem.setUrlCover("http://lorempixel.com/400/200/nightlife/" + String.valueOf(i));
+            result.add(newsItem);
+        }
+        return result;
     }
 }
--- app/src/main/java/com/saraswati/newsapp/ui/main/NewsAdapter.java
+++ app/src/main/java/com/saraswati/newsapp/ui/main/NewsAdapter.java
@@ -1,33 +1,85 @@
 package com.saraswati.newsapp.ui.main;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.saraswati.newsapp.R;
+import com.saraswati.newsapp.data.remote.model.NewsItem;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by saras on 1/14/2018.
  */
 
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
+    private List<NewsItem> dataSet = new ArrayList<>();
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.item_news, parent, false);
+
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
+        NewsItem newsItem = dataSet.get(position);
+        holder.tvTitle.setText(newsItem.getTitle());
+        holder.tvAuthor.setText(newsItem.getAuthor());
+        holder.tvDescription.setText(newsItem.getDescription());
+        //glide untuk mengambil data gambar dari internet
+        Glide.with(holder.itemView.getContext())
+                .load(newsItem.getUrlCover())
+                .into(holder.ivCover);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return dataSet.size();
+
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public void setData(List<NewsItem> newsItemList){
+        this.dataSet = newsItemList;
+        notifyDataSetChanged();
+    }
+
+    public void clearData(){
+        this.dataSet = Collections.emptyList();
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.iv_cover)
+        ImageView ivCover;
+        @BindView(R.id.tv_title)
+        TextView tvTitle;
+        @BindView(R.id.tv_author)
+        TextView tvAuthor;
+        @BindView(R.id.tv_description)
+        TextView tvDescription;
+        @BindView(R.id.btn_read_more)
+        Button btnReadMore;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
         }
     }
 }
